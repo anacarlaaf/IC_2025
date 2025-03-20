9c785fc2b81e33a9f7738cc2203830e1b8b4d007
anacarla <anacarla.daf@gmail.com>
2025-03-20 16:20:05 -0400

    +decreaseKey and deleteKey

--- Binomial Heap/min_bin_heap.cpp
+++ Binomial Heap/min_bin_heap.cpp
@@ -36,6 +36,11 @@ node *findMin(binomialHeap *t){
     return minNode;
 }
 
+bool isEmpty(binomialHeap *b1){
+    if(b1->head!=NULL) return false;
+    else return true;
+}
+
 //priv
 void mergeTrees(binomialHeap *b1, binomialHeap *b2){
     //Merge das árvores em ordem não-decrescente de degree
@@ -84,13 +89,6 @@ void mergeTrees(binomialHeap *b1, binomialHeap *b2){
     b1->head=st;
     b2->head=NULL; // limpar a b2
 }
-
-//priv
-// void linkTrees(node *t1, node *t2){
-//     t2->parent =t1;
-//     t1->child = t2;
-//     t1->degree++;
-// }
 
 void uniteBinomialHeaps(binomialHeap *b1, binomialHeap *b2){
     mergeTrees(b1, b2);
@@ -190,8 +188,23 @@ void extractMin(binomialHeap *t){
     free(ext);
 }
 
-void decreaseKey(binomialHeap *t){
+//O(logn)
+void decreaseKey(binomialHeap *t, node *x, int n){
+    x->key = n;
+    node *y = x, *z = x->parent;
+    int aux;
+    while (z && y->key < z->key){
+        aux = z->key;
+        z->key = n;
+        y->key = aux;
+        y = z;
+        z = y->parent;
+    }
+}
 
+void deleteKey(binomialHeap *t, node *x){
+    decreaseKey(t, x, INT_MIN);
+    extractMin(t);
 }
 
 int main(){
@@ -200,25 +213,22 @@ int main(){
     makeBinomialHeap(&t1);
     makeBinomialHeap(&t2);
 
-    //int v[11] = {7,2,4,17,1,11,6,8,15,10,20}; //ok
-    //int v2[7] = {10, 5, 3, 8, 12, 1, 7}; // ok
+    int v[11] = {7,2,4,17,1,11,6,8,15,10,20}; //ok
+    //int v2[8] = {40, 23, 0, 12, -1, 100, 3, 89}; ok
 
-    //int v3[5] = {9, 3, 6, 12, 15}; // union ok
-    //int v4[6] = {2, 8, 5, 10, 4, 7};
+    for(int i : v) insert(&t1, i);
+    //for(int i : v2) insert(&t2, i);
 
-    int v5[8] = {5, 5, 5, 2, 2, 8, 8, 1}; // ok
+    //mergeTrees(&t1, &t2);
 
-    //int v6[1] = {42}; // ok
+    //decreaseKey(&t1, findMin(&t1)->child->child, 0); //ok
+    //cout << findMin(&t1)->key << "\n";
+    //deleteKey(&t1, findMin(&t1)->child->child); //ok
 
-    //int v7[6] = {10, 8, 6, 4, 2, 0}; //ok
-
-    //int v8[5] = {-3, -1, -5, -2, -4}; //ok
-
-    for(int i : v5) insert(&t1, i);
-
-    for(int i=0;i<8;i++){
+    while(!isEmpty(&t1)){
         cout << "Min: " << findMin(&t1)->key << "\n";
         extractMin(&t1);
     }
+
     return 0;
 }
